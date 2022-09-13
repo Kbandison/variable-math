ab55c824ec86e6340ea5d61bcacfc63d64ff0618
Kevin Bandison <kbandison@gmail.com>
2022-09-13 02:05:43 -0400

    added solutions to adder, future age, greeter, distance and remainder

--- distance.js
+++ distance.js
@@ -1,8 +1,13 @@
 'use strict';
 
 let x1 = 2;
-let y1 = 1;
+let y1 = 4;
 let x2 = 4;
-let y2 = 3;
+let y2 = 6;
 
+let x = x2 - x1;
+let y = y2 - y1;
 
+let result = ((x**2) + (y**2));
+let remainder = Math.ceil(Math.sqrt(result));
+console.log(`The distance between (${x1}, ${y1}) and (${x2}, ${y2}) is ${remainder}.`);
